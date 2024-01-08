b811c4f6ebd9b1357a7aef0d1e543d0f6514446a
Shakoby <119861256+Shakoby@users.noreply.github.com>
2024-01-08 10:12:44 +0000

    01-05 Done

--- start/01_05_private_object.js
+++ start/01_05_private_object.js
@@ -1,1 +1,24 @@
 // Write your code here
+let usr = Symbol('usrSym');
+let pss = Symbol('pssSym');
+
+class user {
+  constructor(userName, password, age) {
+    this[usr] = userName;
+    this[pss] = password;
+    this.age = age;
+  }
+
+  getUserName() {
+    return this[usr];
+  }
+
+  getPassword() {
+    return this[pss];
+  }
+}
+
+const myNew = new user('Hakob', 2122, 21);
+// console.log(myNew[userName]);
+console.log(myNew.age);
+console.log(myNew.getUserName());
