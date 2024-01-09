7c58405b58f6b7f1853e4aaf72bd5546eb42a728
Shakoby <119861256+Shakoby@users.noreply.github.com>
2024-01-09 22:18:51 +0000

    01_12 done

--- start/01_12_string_permutation.js
+++ start/01_12_string_permutation.js
@@ -1,1 +1,30 @@
 // Write your code here
+//factorial(n) = n * n-1 * n-2
+
+
+function factorial(x) {
+  if (x.length === 1) {
+        return 1;
+    }
+
+   return x.length * factorial(x.slice(1));
+
+}
+
+console.log(factorial('hello'));
+
+
+
+
+// function factorial(x) {
+
+//     // if number is 0
+//     if (x == 0) {
+//         return 1;
+//     }
+
+//     // if number is positive
+//     else {
+//         return x * factorial(x - 1);
+//     }
+// }
