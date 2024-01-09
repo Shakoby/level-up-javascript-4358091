e123418a85a900bbc726f8e0cb262afef879663e
Shakoby <119861256+Shakoby@users.noreply.github.com>
2024-01-09 12:09:47 +0000

    01_09 done

--- start/01_09_coffee_calculations.js
+++ start/01_09_coffee_calculations.js
@@ -1,1 +1,14 @@
 // Write your code here
+//reduce
+let pricePerCoffee = 1.25;
+let arr = [2,5,7,4,1];
+
+function calc(arr) {
+    let reduce = arr.reduce((acc, x) => {
+    return acc + x;
+  });
+
+  return str = `The total bill is ${reduce * pricePerCoffee}`;
+}
+
+console.log(calc(arr));
