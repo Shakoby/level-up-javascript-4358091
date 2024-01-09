2aefdbd0b3cf208cf0086bd6f7cbfc6f828ca607
Shakoby <119861256+Shakoby@users.noreply.github.com>
2024-01-09 08:06:21 +0000

    01_07 done

--- start/01_07_classroom_attendance.js
+++ start/01_07_classroom_attendance.js
@@ -1,1 +1,27 @@
 // Write your code here
+//array and object destructuring
+
+//function getStudents(classroom) {}
+//classroom = [Teacher ,TeachingAssistant, classList]
+
+// let classesArr = ['teacher1', 'assTeacher1', 'std1','std2','std3'];
+
+// let hasTeachingAssistant = true;
+// let arr = ['name1', 'name2', 'name3', 'name4' ];
+
+function getStudents(classroom) {
+    let {hasTeachingAssistant, classList} = classroom
+
+  if (hasTeachingAssistant) {
+      [Teacher, AssistantTeacher, ...Students] = classList;
+  } else {
+     [Teacher, ...Students] = classList;
+  }
+
+  return Students;
+};
+
+getStudents({
+  hasTeachingAssistant: false,
+  classList: ["Rashida", "John", "Roman", "Lisa", "Omair", "Lukas"]
+})
